dfad0837b09552afca8b7e6a2b4759220abbafab
Caleb Thompson <cjaysson@gmail.com>
2013-05-06 16:38:16 -0400

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GriddlerDemo::Application.config.secret_token = '963c2b67db726a660cd0863d4cad00a77fe535ff2661a5aedcebbd2a79960d590f1add9256bf3f9c7b0566a2876e6b6fbb6c27813a8c54bc6a46a5e79d0dbc69'
